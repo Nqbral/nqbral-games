6db665b885e3c9439f927ce6f055159f33cf0db4
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-28 19:10:41 +0200

    SERVER - Enable core

--- workspaces/server/src/main.ts
+++ workspaces/server/src/main.ts
@@ -1,8 +1,16 @@
 import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ALLOW_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
