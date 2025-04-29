4250757acc6720929daf65bb7ee7c94c70c952b1
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-29 14:17:11 +0200

    SERVER - configure origins for enable cors

--- workspaces/server/src/main.ts
+++ workspaces/server/src/main.ts
@@ -7,13 +7,18 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(cookieParser());
+  let origins: string[] = [];
+
+  if (process.env.CORS_ALLOW_ORIGIN_NQBRAL_GAMES != undefined) {
+    origins.push(process.env.CORS_ALLOW_ORIGIN_NQBRAL_GAMES);
+  }
+
+  if (process.env.CORS_ALLOW_ORIGIN_LAST_HOPE != undefined) {
+    origins.push(process.env.CORS_ALLOW_ORIGIN_LAST_HOPE);
+  }
+
   app.enableCors({
-    origin: process.env.CORS_ALLOW_ORIGIN_NQBRAL_GAMES,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  app.enableCors({
-    origin: process.env.CORS_ALLOW_ORIGIN_LAST_HOPE,
+    origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
