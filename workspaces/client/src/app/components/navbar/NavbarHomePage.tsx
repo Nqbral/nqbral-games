96b7eb9fb7e2a674e4f136e9119ded5532cd72c1
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-29 11:05:32 +0200

    CLIENT - Reset error message login when pushing signin page from home navbar

--- workspaces/client/src/app/components/navbar/NavbarHomePage.tsx
+++ workspaces/client/src/app/components/navbar/NavbarHomePage.tsx
@@ -5,9 +5,16 @@ import LinkButton from '@components/buttons/LinkButton';
 import NqbralGamesLogo from '@public/nqbral-games-logo-row.png';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function NavbarHomePage() {
-  const { isLogged, user } = useAuth();
+  const { resetError, isLogged, user } = useAuth();
+  const router = useRouter();
+
+  const toLogin = () => {
+    resetError();
+    router.push('/signin');
+  };
 
   return (
     <div className="fixed top-0 flex w-full flex-row items-center justify-between bg-neutral-900 px-6 py-4 shadow-sm shadow-neutral-950">
@@ -19,7 +26,12 @@ export default function NavbarHomePage() {
         <LinkButton href="/profile/informations" buttonText={user?.username} />
       )}
       {isLogged == false && (
-        <LinkButton href="/signin" buttonText="Se connecter" />
+        <button
+          className="transition-colors hover:text-neutral-300"
+          onClick={toLogin}
+        >
+          Se connecter
+        </button>
       )}
     </div>
   );
