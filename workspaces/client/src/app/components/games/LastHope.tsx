e9168c17b7cce0fa9c7d6d8b0c18c96e0a163060
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-30 11:36:22 +0200

    CLIENT - change size logo last hope on home page

--- workspaces/client/src/app/components/games/LastHope.tsx
+++ workspaces/client/src/app/components/games/LastHope.tsx
@@ -16,7 +16,7 @@ export default function LastHope() {
       }
     >
       <div className="flex w-full flex-col items-center border-b-1 border-slate-700 py-4">
-        <Image src={LastHopeLogo} alt="last-hope-logo" className="w-80" />
+        <Image src={LastHopeLogo} alt="last-hope-logo" className="w-52" />
       </div>
       <div className="flex w-full flex-col items-center gap-4 px-8 py-4">
         <div className="text-center">
