fba32bbbe6cdf867744994eaea8bbc55604b104d
Simon Chevalier <sim.chevalier@gmail.com>
2025-06-27 18:35:00 +0200

    CLIENT - images header cache

--- workspaces/client/next.config.ts
+++ workspaces/client/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: '/:all*.(jpg|jpeg|png|gif|ico|webp|svg)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
